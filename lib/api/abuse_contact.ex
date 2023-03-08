f69448df0cc14edc292104d44c1c5adbcf914d0b
hertogp <git.hertogp@gmail.com>
2023-03-08 08:16:23 +0100

    anonymize

--- lib/api/abuse_contact.ex
+++ lib/api/abuse_contact.ex
@@ -3,51 +3,52 @@ defmodule Ripe.API.AbuseContact do
   See https://stat.ripe.net/docs/02.data-api/abuse-contact-finder.html
   """
 
-  # %Tesla.Env{
-  #   method: :get,
-  #   url: "https://stat.ripe.net/data/abuse-contact-finder/data.json?sourceapp=github-ripex&resource=42894",
-  #   query: [],
-  #   headers: [
-  #     {"server", "nginx"},
-  #     {"date", "Tue, 07 Mar 2023 07:32:50 GMT"},
-  #     {"content-type", "application/json; charset=utf-8"},
-  #     {"content-length", "739"},
-  #     {"connection", "keep-alive"},
-  #     {"vary", "Cookie, Accept-Encoding"},
-  #     {"access-control-allow-origin", "*"},
-  #     {"x-frame-options", "SAMEORIGIN"},
-  #     {"x-xss-protection", "1; mode=block"},
-  #     {"x-content-type-options", "nosniff"},
-  #     {"permissions-policy", "interest-cohort=()"},
-  #     {"strict-transport-security", "max-age=31536000; includeSubdomains"}
-  #   ],
-  #   body: %{
-  #     "build_version" => "live.2023.2.1.142",
-  #     "cached" => false,
-  #     "data" => %{
-  #       "abuse_contacts" => ["gilbert.jimenez@rws.nl"],
-  #       "authoritative_rir" => "ripe",
-  #       "earliest_time" => "2023-03-07T07:32:50",
-  #       "latest_time" => "2023-03-07T07:32:50",
-  #       "parameters" => %{"cache" => nil, "resource" => "42894"}
-  #     },
-  #     "data_call_name" => "abuse-contact-finder",
-  #     "data_call_status" => "supported",
-  #     "messages" => [],
-  #     "process_time" => 60,
-  #     "query_id" => "20230307073250-c900d25f-a5e2-427e-82d3-26cc51b2cd59",
-  #     "see_also" => [],
-  #     "server_id" => "app112",
-  #     "status" => "ok",
-  #     "status_code" => 200,
-  #     "time" => "2023-03-07T07:32:50.294954",
-  #     "version" => "2.1"
-  #   },
-  #   status: 200,
-  #   opts: [],
-  #   __module__: Ripe.API,
-  #   __client__: %Tesla.Client{fun: nil, pre: [], post: [], adapter: nil}
-  # }
+  # {:ok,
+  #  %Tesla.Env{
+  #    method: :get,
+  #    url: "https://stat.ripe.net/data/abuse-contact-finder/data.json?sourceapp=github-ripex&resource=333",
+  #    query: [],
+  #    headers: [
+  #      {"server", "nginx"},
+  #      {"date", "Wed, 08 Mar 2023 07:15:26 GMT"},
+  #      {"content-type", "application/json; charset=utf-8"},
+  #      {"content-length", "764"},
+  #      {"connection", "keep-alive"},
+  #      {"vary", "Cookie, Accept-Encoding"},
+  #      {"access-control-allow-origin", "*"},
+  #      {"x-frame-options", "SAMEORIGIN"},
+  #      {"x-xss-protection", "1; mode=block"},
+  #      {"x-content-type-options", "nosniff"},
+  #      {"permissions-policy", "interest-cohort=()"},
+  #      {"strict-transport-security", "max-age=31536000; includeSubdomains"}
+  #    ],
+  #    body: %{
+  #      "build_version" => "live.2023.2.1.142",
+  #      "cached" => false,
+  #      "data" => %{
+  #        "abuse_contacts" => ["disa.columbus.ns.mbx.arin-registrations@mail.mil"],
+  #        "authoritative_rir" => "arin",
+  #        "earliest_time" => "2023-03-08T07:15:26",
+  #        "latest_time" => "2023-03-08T07:15:26",
+  #        "parameters" => %{"cache" => nil, "resource" => "333"}
+  #      },
+  #      "data_call_name" => "abuse-contact-finder",
+  #      "data_call_status" => "supported",
+  #      "messages" => [],
+  #      "process_time" => 425,
+  #      "query_id" => "20230308071525-c62eeeaf-6917-4e1f-b891-86b2467824bf",
+  #      "see_also" => [],
+  #      "server_id" => "app133",
+  #      "status" => "ok",
+  #      "status_code" => 200,
+  #      "time" => "2023-03-08T07:15:26.035829",
+  #      "version" => "2.1"
+  #    },
+  #    status: 200,
+  #    opts: [],
+  #    __module__: Ripe.API,
+  #    __client__: %Tesla.Client{fun: nil, pre: [], post: [], adapter: nil}
+  #  }}
 
   alias Ripe.API
 
