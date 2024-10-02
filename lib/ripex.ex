04fac1e7a3c5cf40781dd96aa60f762daa4c29fa
hertogp <git.hertogp@gmail.com>
2024-10-02 22:16:49 +0200

    docs

--- lib/ripex.ex
+++ lib/ripex.ex
@@ -2,16 +2,52 @@ defmodule Ripex do
   @moduledoc """
   Documentation for `Ripex`.
 
-  API's
-  - Ripe.Stat
+  # API's
+
+  ## [Ripe Stat](https://stat.ripe.net/docs/02.data-api/)
     * Ripe.Stat.API.endpoint(s)
 
-  - Ripe.DB
+  ## [Ripe Database](https://docs.db.ripe.net/Update-Methods/RESTful-API/)
     + Ripe.DB.lookup
       * Ripe.DB.Lookup.endpoint(s)
-
     + Ripe.DB.search
       * Ripe.DB.Search.endpoint(s)
+
+  ### Primary objects
+
+  - [AUT-NUM](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Primary-Objects/#description-of-the-aut-num-object)
+  - [DOMAIN](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Primary-Objects/#description-of-the-domain-object)
+  - [INET6NUM](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Primary-Objects/#description-of-the-inet6num-object)
+  - [INETNUM](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Primary-Objects/#description-of-the-inetnum-object)
+  - [ROUTE](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Primary-Objects/#description-of-the-route-object)
+  - [ROUTE6](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Primary-Objects/#description-of-the-route6-object)
+  - [AS-SET](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Primary-Objects/#description-of-the-as-set-object)
+  - [FILTER-SET](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Primary-Objects/#description-of-the-filter-set-object)
+  - [INET-RTR](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Primary-Objects/#description-of-the-inet-rtr-object)
+  - [PEERING-SET](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Primary-Objects/#description-of-the-peering-set-object)
+  - [ROUTE-SET](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Primary-Objects/#description-of-the-route-set-object)
+  - [RTR-SET](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Primary-Objects/#description-of-the-rtr-set-object)
+
+  ### Secondary objects
+
+  - [AS-BLOCK](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Secondary-Objects/#description-of-the-as-block-object)
+  - [IRT](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Secondary-Objects/#description-of-the-irt-object)
+  - [KEY-CERT](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Secondary-Objects/#description-of-the-key-cert-object)
+  - [MNTNER](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Secondary-Objects/#description-of-the-mntner-object)
+  - [ORGANISATION](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Secondary-Objects/#description-of-the-organisation-object)
+  - [PERSON](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Secondary-Objects/#description-of-the-person-object)
+  - [POEM](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Secondary-Objects/#description-of-the-poem-object)
+  - [POETIC-FORM](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Secondary-Objects/#description-of-the-poetic-form-object)
+  - [ROLE](https://docs.db.ripe.net/RPSL-Object-Types/Descriptions-of-Secondary-Objects/#description-of-the-role-object)
+
+
+  ## [RIPE IP map](https://ipmap.ripe.net/docs/02.api-reference/)
+
+  [ ] todo, if useful
+
+  ## [RIPE Atlas](https://atlas.ripe.net/docs/apis/rest-api-manual/)
+
+  [ ] todo, if useful
 
   lib/ripe                       - api.ex utilities for all API's + general documentation
   lib/ripe/api
@@ -29,17 +65,4 @@ defmodule Ripex do
   lib/ripe/api/rdap/endpoints(s)
 
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Ripex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
